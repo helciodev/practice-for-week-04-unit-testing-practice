976063fc5bed8bfbe1934b1ee9a807b1fcbf54fe
helcio <h.andre@giap.pl>
2023-12-11 12:52:00 +0100

    Complete tests

--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,34 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  beforeEach(() => {
+    newWord = new Word("world");
+  });
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(newWord.word).exist;
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(newWord.word).to.equal("world");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(newWord.removeVowels()).to.equal("wrld");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(newWord.removeConsonants()).to.equal("o");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(newWord.pigLatin()).to.equal("orldway");
     });
   });
 });
